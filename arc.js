906d3fb38519afe1e1c349e5f60dcc77878588a4
Dasha11 <dashajq@gmail.com>
2016-04-21 22:55:39 +0600

    arc

--- arc.js
+++ arc.js
@@ -7,7 +7,16 @@ new Vue({
 		bally:0,
 		boardPosition:200,
 		boardWidth:100,
-		fieldWidth:600
+		fieldWidth:600,
+		fieldHeigth:400,
+		step:25,
+		interval:10,
+		dirx:1,
+		diry:1,
+		intervalId:null,
+		ballWigth:50,
+		ballHeigth:50,
+		boardHeigth:20
 		}
 		
 	},
@@ -19,8 +28,30 @@ new Vue({
 			}
 			if(this.boardWidth+this.boardPosition>this.fieldWidth)
 				this.boardPosition=this.fieldWidth-this.boardWidth;
+	
+	},	
+	onClick: function(){
+		var self=this;
+		if(this.intervalId==null){
+			this.intervalId=setInterval(function(){
+				self.ballx+=self.step*self.dirx;
+				self.bally+=self.step*self.diry;
+				if(self.ballx>=self.fieldWidth-self.ballWigth||self.ballx<=0){
+					self.dirx=self.dirx*(-1);
+				}if(self.bally<=0){
+					self.diry=self.diry*(-1);
+				}if(self.bally>=self.fieldHeigth-self.ballHeigth){
+					var ballPosition=self.ballx+self.ballWigth/2;
+					if(ballPosition>=self.boardPosition && ballPosition<=self.boardWidth+self.boardPosition){
+						self.diry=self.diry*(-1);
+					}else{
+						clearInterval(self.intervalId);
+					}
+				}
+			},200)
+		}
 	}
-	}	
-		
-  })
+		}
+	});
+	
 });
